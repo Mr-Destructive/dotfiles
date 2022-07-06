41a7159421ff8403a6480ed8b423f1af37ceedc7
mr_destructive <gormeet711@gmail.com>
2022-07-06 14:07:57 +0530

    feat: lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,8 @@
 require('destructive')
 require('destructive/packer')
+require('destructive/lsp_config')
 require('destructive/keymap')
+require('destructive/set')
 require('destructive/color')
 
-vim.g.mapleader = ' '
-local set = vim.opt
-
-set.tabstop = 2
-set.shiftwidth = 2
-set.softtabstop = 2
-set.expandtab = true
-
-vim.keymap.set('n', '<Leader>w', ':w<CR>')
-vim.keymap.set('n', '<Leader>p', '"+p')
-vim.keymap.set('n', '<Leader>q', '":q!')
-vim.keymap.set('n', '<Leader>ev', ':vsplit $MYVIMRC<CR>')
-vim.keymap.set('n', '<Leader>ev', ':vsplit $MYVIMRC<CR>')
-vim.keymap.set('n', '<Leader>sv', ':w<cr>:so %<cr>:q<cr>')
-vim.keymap.set('n', '<C-n>', ' :NERDTree<CR>')
-
+vim.api.nvim_command('set number')
--- nvim/lua/destructive/color.lua
+++ nvim/lua/destructive/color.lua
@@ -1,1 +1,8 @@
-vim.cmd('colorscheme dracula')
+vim.cmd("colorscheme dracula")
+vim.api.nvim_command([[
+    augroup ChangeBackgroudColour
+        autocmd colorscheme * :hi normal guibg=#00000f guifg=#ffffff
+    augroup END
+]])
+vim.o.termguicolors = true
+vim.cmd [[silent! colorscheme snow]]
