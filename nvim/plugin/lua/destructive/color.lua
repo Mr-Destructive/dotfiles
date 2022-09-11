12635bd817c896be44df28c6e05d87b2ec60f226
mr_destructive <gormeet711@gmail.com>
2022-09-11 18:37:14 +0530

    fix: redundant plugin folder

--- nvim/plugin/lua/destructive/color.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_command([[
-    augroup ChangeBackgroudColour
-        autocmd colorscheme * :hi normal guibg=#000030 guifg=#ffffff
-        autocmd colorscheme * :hi Directory guifg=#00FFFF ctermfg=#ffffff
-    augroup END
-]])
-vim.o.termguicolors = true
-vim.cmd [[silent! colorscheme dracula]]
--- nvim/plugin/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("destructive")
---require('destructive.packer')
---require('destructive.lsp_config')
---require('destructive.set_map')
---require('destructive.color')
